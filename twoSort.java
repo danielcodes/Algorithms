36c5f75838ae6c94619bc0fc02fba31a5df9049f
Daniel Chia <daniel_1118@hotmail.com>
2015-03-06 00:51:31 -0800

    getting started

---
+++ twoSort.java
@@ -0,0 +1,11 @@
+public class twoSort
+{
+    public static void main(String[] args)
+    {
+        //method that combines merge and insertion sort
+        //for better efficiency
+        System.out.println("TESTING");
+
+    }
+
+}
