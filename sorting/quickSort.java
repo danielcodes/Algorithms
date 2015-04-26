5d16c8b6efc567b8cfa23b1b15653f26ccb7323e
Daniel Chia <daniel_1118@hotmail.com>
2015-04-26 14:44:17 -0700

    need to finish partition for quick

--- sorting/quickSort.java
+++ sorting/quickSort.java
@@ -4,39 +4,39 @@ public class quickSort
     {
         System.out.println("hello noob");
 
+        int[] array = {4,5,3,2,1};
+
+        quickSort(array, 0, 4);
+
+        for(int i=0; i<array.length; i++)
+        {
+            System.out.println(array[i] + " ");
+        }
 
     }
 
-
-    int partition(int arr[], int left, int right)
+    //parameters, the array,, beginning and # of ele - 1
+    //end is array.length - 1
+    public static void quickSort(int start, int end, int[] array)
     {
-          int i = left, j = right;
-          int tmp;
-          int pivot = arr[(left + right) / 2];
-
-          while (i <= j) {
-                while (arr[i] < pivot)
-                      i++;
-                while (arr[j] > pivot)
-                      j--;
-                if (i <= j) {
-                      tmp = arr[i];
-                      arr[i] = arr[j];
-                      arr[j] = tmp;
-                      i++;
-                      j--;
-                }
-          };
-
-          return i;
+        int pivot = partition(start, end, array);
+        quickSort(start, pivot - 1, array);
+        quickSort(pivot + 1, end, array);
     }
 
-    void quickSort(int arr[], int left, int right)
+    //partition
+    public static int partition(int start, int end, int[] array)
     {
-          int index = partition(arr, left, right);
-          if (left < index - 1)
-                quickSort(arr, left, index - 1);
-          if (index < right)
-                quickSort(arr, index, right);
+        //i and j, start at the beginning
+        //loop ends when j reaches the end-1, and the pivot is then
+        //swapped with i's last position
+        int i = 0, j = 0;
+        int temp;
+        int pivot = array[end];
+
+        
+
+
 
     }
+}
